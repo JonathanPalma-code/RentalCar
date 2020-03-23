0129615f0cd1c616aaa007f7a4b8115584bc2fd6
Jonathan <jonathanpalma89@gmail.com>
2020-03-23 16:46:07 +0000

    chore: add Entities: Vehicle, Invoice and CarRental

---
+++ Entities/CarRental.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentalCar.Entities
+{
+    class CarRental
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime startDate, DateTime endDate, Vehicle vehicle)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Vehicle = vehicle;
+        }
+    }
+}
---
+++ Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace RentalCar.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
